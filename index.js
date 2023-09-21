5ed99b2f513c3ce83f9f4fef94ad9518b94f9a46
prasadbabu3234 <prasadbabuyanbu@gmail.com>
2023-09-21 13:33:52 +0530

    change

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { MongoClient,ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
 const jwt = require('jsonwebtoken')
 // const {PORT}  = require('dotenv')
 const cors = require('cors')
 const app = express();
+const fs = require('fs');
+const { ObjectId } = require('mongodb');
+
+const imageBuffer = fs.readFileSync('./Prasad babu.jpeg');
 
 
 
@@ -14,37 +18,38 @@ app.use(express.json())
 
 const url = "mongodb+srv://prasadbabuyanbu:prasad3234@cluster0.wdccmqo.mongodb.net/?retryWrites=true&w=majority";
 
+
 const client = new MongoClient(url, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-  });
+});
 
 
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.send("hello")
 })
 
-app.post('/login',async (req, res) => {
+app.post('/login', async (req, res) => {
 
     try {
         const receivedData = req.body; // This will contain the data sent from Angular
         const collection1 = client.db('UserDetails').collection('Users');
-        const allData = await collection1.find({ "$or": [{ "mobile": receivedData.mobile }, { "email": receivedData.mobile },{"password":receivedData.password}] }).toArray();
+        const allData = await collection1.find({ "$or": [{ "mobile": receivedData.mobile }, { "email": receivedData.mobile }, { "password": receivedData.password }] }).toArray();
         if (allData.length > 0) {
             if (allData[0].mobile == receivedData.mobile || allData[0].email === receivedData.mobile) {
                 if (allData[0].password === receivedData.password) {
                     let data = receivedData.mobile
                     let token = jwt.sign(data, 'myToken')
-                    res.send({token:token,allData})
+                    res.send({ token: token, allData })
                 } else {
                     res.status(403).json({ message: 'Incorrect passoword' })
                 }
-            }else{
-                res.status(403).json({message:"Incorrect Email or Mobile"})
+            } else {
+                res.status(403).json({ message: "Incorrect Email or Mobile" })
             }
         }
         else {
@@ -59,7 +64,7 @@ app.post('/login',async (req, res) => {
 app.post("/register", async (req, res) => {
 
     try {
-        const receivedData = req.body; 
+        const receivedData = req.body;
         const collection1 = client.db('UserDetails').collection('Users');
         const allData = await collection1.find({ "$or": [{ "mobile": receivedData.mobile }, { "email": receivedData.email }] }).toArray();
         if (allData.length == 0) {
@@ -78,6 +83,36 @@ app.post("/register", async (req, res) => {
         console.log(error)
     }
 });
+
+app.post('/profile', async (req, res) => {
+    try {
+        const collection1 = client.db('Mrriage-buearo').collection('Marriage_Details');
+
+        const update = await collection1.updateOne(
+            { _id: new ObjectId("650976adbfa106c424242dd5") },
+            {
+                $set: {
+                    imageName: 'example.jpg',
+                    imageData: imageBuffer,
+                }
+            }
+        )
+        console.log("inserted successfully")
+        res.send("inserted successfully")
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get('/profile', async (req,res) => {
+    try {
+        const collection1 = client.db('Mrriage-buearo').collection('Marriage_Details');
+        const result = await collection1.find({}).toArray()
+        res.send(result)
+    } catch (error) {
+        
+    }
+})
 
 app.listen(4000, async () => {
     try {
